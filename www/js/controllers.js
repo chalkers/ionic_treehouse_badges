4a5d79103f1909fbb9e110933deacd79ca41694b
Andrew Chalkley <andrew@chalkley.org>
2015-06-07 22:14:09 -0700

    initial app

---
+++ www/js/controllers.js
@@ -0,0 +1,16 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $http) {
+  //Get profile information
+  $http.get('https://teamtreehouse.com/chalkers.json').
+  success(function(data, status, headers, config) {
+    // this callback will be called asynchronously
+    // when the response is available
+    $scope.badges = data.badges;
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+  
+});
