e40ad200aa588ebd4ef9bc7a93a9920113181be2
Andrew Chalkley <andrew@chalkley.org>
2015-06-07 22:23:36 -0700

    Added loading

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,19 @@
 angular.module('starter.controllers', [])
 
-.controller('BadgesCtrl', function($scope, $http) {
+.controller('BadgesCtrl', function($scope, $http, $ionicLoading) {
+   $ionicLoading.show({
+      template: 'Retrieving Badge Information...'
+    });
   //Get profile information
   $http.get('https://teamtreehouse.com/chalkers.json').
   success(function(data, status, headers, config) {
+     $ionicLoading.hide();
     // this callback will be called asynchronously
     // when the response is available
     $scope.badges = data.badges;
   }).
   error(function(data, status, headers, config) {
+    $ionicLoading.hide();
     // called asynchronously if an error occurs
     // or server returns response with an error status.
   });
